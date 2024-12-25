6a3444332a4bb8588c434743be407de4632bae3f
18532672238 <canyonwan@126.com>
2024-12-25 16:50:14 +0800

    refactor: remove files

--- pages.config.ts
+++ pages.config.ts
@@ -20,42 +20,30 @@ export default defineUniPages({
         'z-paging/components/z-paging$1/z-paging$1.vue',
     },
   },
-  tabBar: {
-    custom: true,
-    color: '#999999',
-    selectedColor: '#018d71',
-    backgroundColor: '#F8F8F8',
-    borderStyle: 'black',
-    height: '50px',
-    fontSize: '10px',
-    iconWidth: '24px',
-    spacing: '3px',
-    list: [
-      // 注意tabbar路由需要使用 layout:tabbar 布局
-      {
-        pagePath: 'pages/index/index',
-        text: '首页',
-        icon: 'home',
-        iconType: 'wot',
-      },
-      {
-        pagePath: 'pages/about/about',
-        text: '关于',
-        icon: 'i-carbon-code',
-        iconType: 'unocss',
-      },
-      // {
-      //   pagePath: 'pages/my/index',
-      //   text: '我的',
-      //   icon: '/static/logo.svg',
-      //   iconType: 'local',
-      // },
-      {
-        pagePath: 'pages/my/index',
-        text: '我的',
-        icon: 'iconfont icon-my',
-        iconType: 'iconfont',
-      },
-    ],
-  },
+  // tabBar: {
+  //   custom: true,
+  //   color: '#999999',
+  //   selectedColor: '#018d71',
+  //   backgroundColor: '#F8F8F8',
+  //   borderStyle: 'black',
+  //   height: '50px',
+  //   fontSize: '10px',
+  //   iconWidth: '24px',
+  //   spacing: '3px',
+  //   list: [
+  //     // 注意tabbar路由需要使用 layout:tabbar 布局
+  //     {
+  //       pagePath: 'pages/index/index',
+  //       text: '首页',
+  //       icon: 'home',
+  //       iconType: 'wot',
+  //     },
+  //     {
+  //       pagePath: 'pages/my/index',
+  //       text: '我的',
+  //       icon: 'iconfont icon-my',
+  //       iconType: 'iconfont',
+  //     },
+  //   ],
+  // },
 })
